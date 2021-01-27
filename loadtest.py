9df73b3d034ba197a01b334fa062495c6743c3c8
Keith Miller <keithemmanuel@gmail.com>
2021-01-27 13:52:24 -0500

    Tested code and fixed errors for POST

--- loadtest.py
+++ loadtest.py
@@ -12,9 +12,10 @@ def post_test():
     messages = int(input('Enter number of messages to generate'))
     
     for i in range(messages):
-        url = 'http://127.0.0.1:5000/author-quotes/{id}'
+        url = 'http://127.0.0.1:5000/author-quotes/' + str(id)
         params ={'author': fake.name(), 'quote': fake.text()}
         post_message = requests.post(url, data = params)
+        print(url)
         print(post_message.text)
         id += 1
 main()
